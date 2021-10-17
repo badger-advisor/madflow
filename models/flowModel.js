ca178d91a6b4ce3c8e834fbe8552d68cda750338
James Zhang <jameszhanga@gmail.com>
2021-10-17 14:42:21 -0500

    Flow model complete

--- models/flowModel.js
+++ models/flowModel.js
@@ -1,6 +1,20 @@
 const mongoose = require('mongoose');
 
 const flowSchema = new mongoose.Schema({
-  name     : String,
-  elements : Array
+  name     : {
+    type     : String,
+    required : [ true, 'A flow must have a name' ],
+    unique   : true
+  },
+  // Since elements will have a bunch of different things in it
+  // using type Array will initialize an array of [Mixed] types
+  elements : Array,
+  major    : {
+    type     : String,
+    required : [ true, 'Each flow must have a major specified' ]
+  }
 });
+
+const Flow = mongoose.model('flow', flowSchema);
+
+module.exports(Flow);
