00f793ae40536fd1e357378de95529d3bffcfc22
JaskaranBakshi <jaskaran@quiverquant.com>
2021-10-18 15:24:40 -0500

    schemas

---
+++ models/courseModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const courseSchema = new mongoose.Schema({
+  courseInfo     : {
+    type     : String,
+    required : [ true, 'A course must have info' ],
+    unique   : true
+  }
+  prerequisites : Array,
+});
+
+const Course = mongoose.model('user', courseSchema);
+
+module.exports(Course);
--- models/userModel.js
+++ models/userModel.js
@@ -3,19 +3,19 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name     : {
     type     : String,
-    required : [ true, 'A flow must have a name' ],
+    required : [ true, 'A user must have a name' ],
     unique   : true
   },
 
   email : {
     type     : String,
-    required : [ true, 'A flow must have a name' ],
+    required : [ true, 'A user must have an email' ],
     unique   : true
   }
   flows : Array,
   majors    : Array
 });
 
-const Flow = mongoose.model('user', userSchema);
+const User = mongoose.model('user', userSchema);
 
-module.exports(Flow);
+module.exports(User);
