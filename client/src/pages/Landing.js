abbb71de48367a82daf6d14a3539edcb7689e31f
Gene <galee2@wisc.edu>
2021-10-18 14:26:39 -0500

    makeStyles() help

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,13 +1,37 @@
-import { Button, Typography } from '@mui/material';
+import { Button, Typography, Box } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles(() => {
+  return {
+    root     : {
+      display         : 'flex',
+      backgroundColor : '#f7f7f7',
+      width           : '100%',
+      height          : '100%'
+    },
+    sidePane : {
+      display         : 'flex',
+      backgroundColor : '#f7f7f7',
+      //alpha('000000', 0.17)
+      width           : '300px',
+      height          : '100%'
+    },
+    guest    : {
+      backgroundColor : 'AE2012',
+      color2          : '0A9396'
+    }
+  };
+});
 
 const Landing = () => {
+  const classes = useStyles();
+  //className={classes.sidePane}
   return (
     <div>
-      hello
-      <Typography>Welcome to </Typography>
-      <Typography color='red'>Mad</Typography>
-      <Typography>Flow</Typography>
-      <Button variant='outlined'>Hello</Button>
+      <Typography variant='h3' align='center'>
+        Welcome to Mad Flow
+      </Typography>
+      <Button variant='outlined'>Sign in with Google</Button>
     </div>
   );
 };
