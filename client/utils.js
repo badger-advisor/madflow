e4a7209411622fdaf83943339e764f9cd07002e2
James Zhang <jameszhanga@gmail.com>
2021-10-24 04:03:03 -0500

    Function to generate node from course data

---
+++ client/utils.js
@@ -0,0 +1,17 @@
+import { isNode, isEdge, removeElements, addEdge } from 'react-flow-renderer';
+
+/**
+ * Genereates a Node from raw course data
+ * @param {Object} courseData The exact course object retrieved from the DB
+ * @returns a Node object ready to be inserted to the Flow
+ */
+export const generateNode = courseData => {
+  const { info, courseNumber, prerequisites } = courseData;
+
+  return {
+    id       : courseNumber,
+    position : { x: 0, y: 0 },
+    type     : 'course',
+    data     : { label: courseNumber, prerequisites, ...info }
+  };
+};
