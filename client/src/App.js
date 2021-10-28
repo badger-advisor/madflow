93aaaa20a462e7f47818bcb792d2b48955f88b90
James Zhang <jameszhanga@gmail.com>
2021-10-27 22:32:01 -0500

    Fix index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Landing, Dashboard, Profile, Graph } from './pages';
-import ApiTests from './components/ApiTests';
 
 const App = () => {
   return (
--- client/src/index.js
+++ client/src/index.js
@@ -1,25 +1,16 @@
-import axios from 'axios';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const API = axios.create({ baseURL: 'http://localhost:8080' });
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-// API.interceptors.request.use((req) => {
-//   if (localStorage.getItem('profile')) {
-//     req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
-//   }
-//   return req;
-// }); // ???
-
-export const signIn = (formData) => API.post('/user/signin', formData);
-export const signUp = (formData) => API.post('/user/signup', formData);
-
-export const getAllUserFlows = (userID) => API.get(`/flow/${userID}`);
-export const getUserFlow = (userID, flowID) => API.get(`/flow/${userID}/${flowID}`);
-export const deleteUserFlow = (userID, flowID) => API.delete(`/flow/${userID}/${flowID}`);
-export const createUserFlow = (userID, flowID, flow) => API.post(`/flow/${userID}/${flowID}`, flow);
-export const updateUserFlow = (userID, flowID, updatedUserFlow) =>
-  API.patch(`/flow/${userID}/${flowID}`, updatedUserFlow);
-export const getPrefilledFlow = (majorID) => API.get(`/flow/prefilled/${majorID}`);
-
-export const deleteUser = (userID) => API.delete(`/user/${userID}`);
-
-export const getCourse = (courseNumber) => API.get(`/course/${courseNumber}`);
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
