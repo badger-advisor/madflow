91dac1540490dc3156ef9d16b022dba38b50ffc8
James Zhang <83667312+jameszhang-a@users.noreply.github.com>
2021-10-29 11:59:49 -0500

    Get graph feature from dev (#12)
    * Finishe Dashboard UI (#10)
    
    * Add placeholder icons to app bar, autocomplete search bar, and minimap
    
    * Change color scheme
    
    * Refactor code
    
    * setup initial grid for FlowCards
    
    * Fixes to grid that holds FlowCards
    
    * initial styling for FlowCard
    
    * Setup initial grid for FlowCards
    
    * Fixes to grid that holds FlowCards
    
    * initial styling for FlowCard
    
    * changes to display image and flow name
    
    * adds menu button to FlowCard
    
    * adds clicks to menu icon and FlowCard
    
    * adds menu to FlowCard
    
    * create FlowCardMenu with dummy menu functions
    
    * fixes to NavBar: temp logo, profile, new button
    
    * skeleton functions for nav bar buttons
    
    * create menu for profile icon
    
    * make View Profile menu option linked
    
    * trying to move menu '...' button
    
    Co-authored-by: gjsorrell <gregorysorrell@gmail.com>
    
    * Complete graph page UI (#11)
    
    * Add placeholder icons to app bar, autocomplete search bar, and minimap
    
    * Change color scheme
    
    * Refactor code
    
    * Partially complete Course Addition popup
    
    * Updated the behavior for the course addition popup
    
    * Fixed zIndex bug with course addition popup
    
    * Added option to show more text to Course Addition popup, updated styling
    
    * Added Custom Node types, initial elements
    
    * Moved Course Node styles to a new file
    
    Co-authored-by: James Zhang <83667312+jameszhang-a@users.noreply.github.com>
    
    Co-authored-by: galee2 <55897917+galee2@users.noreply.github.com>
    Co-authored-by: gjsorrell <gregorysorrell@gmail.com>
    Co-authored-by: gjsorrell <43820133+gjsorrell@users.noreply.github.com>

--- client/src/components/GraphPage/Flow.js
+++ client/src/components/GraphPage/Flow.js
@@ -1,84 +1,181 @@
-import React from 'react'
+import React from 'react';
 import { useState, useRef } from 'react';
 import ReactFlow, {
-  ReactFlowProvider,
-  addEdge,
-  removeElements,
-  Controls,
-  Background,
-  MiniMap
+	ReactFlowProvider,
+	addEdge,
+	removeElements,
+	Controls,
+	Background,
+	Handle
 } from 'react-flow-renderer';
 
+import './dnd.css';
+import CourseNodeStyles from './CourseNodeStyles';
+
 const initialElements = [
-  {
-    id: '1',
-    type: 'input',
-    data: { label: 'Welcome to MadFlow!' },
-    position: { x: 800, y: 100 },
-  },
+	{
+		id: '1',
+		type: 'courseTaken',
+		data: { label: 'Welcome to MadFlow!' },
+		position: { x: 795, y: 105 }
+	},
+	{
+		id: '2',
+		type: 'courseTaken',
+		data: { label: 'CS 400' },
+		position: { x: 690, y: 210 }
+	},
+	{
+		id: '3',
+		type: 'courseCanTake',
+		data: { label: 'CS 240' },
+		position: { x: 900, y: 210 }
+	},
+	{
+		id: '4',
+		type: 'courseCannotTake',
+		data: { label: 'CS 577' },
+		position: { x: 795, y: 315 }
+	},
+
+	{
+		id: 'e1-2',
+		source: '1',
+		target: '2',
+		type: 'smoothstep',
+		label: 'Course planning...',
+		labelBgStyle: { fill: '#f7f7f7' },
+		arrowHeadType: 'arrowclosed'
+	},
+	{
+		id: 'e1-3',
+		source: '1',
+		target: '3',
+		type: 'smoothstep',
+		animated: 'true',
+		label: '...made easy!',
+		labelBgStyle: { fill: '#f7f7f7' },
+		arrowHeadType: 'arrowclosed'
+	},
+	{
+		id: 'e2-4',
+		source: '2',
+		target: '4',
+		type: 'smoothstep',
+		animated: 'true',
+		arrowHeadType: 'arrowclosed'
+	},
+	{
+		id: 'e3-4',
+		source: '3',
+		target: '4',
+		type: 'smoothstep',
+		style: { stroke: '#a33d3d' },
+		animated: 'true',
+		arrowHeadType: 'arrowclosed',
+		arrowHeadColor: '#a33d3d'
+	}
 ];
 
 let id = 0;
 const getId = () => `dndnode_${id++}`;
 
 const Flow = () => {
-  //State related to React Flow
-  const reactFlowWrapper = useRef(null);
-  const [reactFlowInstance, setReactFlowInstance] = useState(null);
-  const [elements, setElements] = useState(initialElements);
-  const onConnect = (params) => setElements((els) => addEdge(params, els));
-  const onElementsRemove = (elementsToRemove) =>
-    setElements((els) => removeElements(elementsToRemove, els));
+  const styles = useState(CourseNodeStyles);
+	const reactFlowWrapper = useRef(null);
+	const [ reactFlowInstance, setReactFlowInstance ] = useState(null);
+	const [ elements, setElements ] = useState(initialElements);
+	const onConnect = (params) => setElements((els) => addEdge(params, els));
+	const onElementsRemove = (elementsToRemove) => setElements((els) => removeElements(elementsToRemove, els));
+	const onLoad = (_reactFlowInstance) => setReactFlowInstance(_reactFlowInstance);
 
-  const onLoad = (_reactFlowInstance) =>
-    setReactFlowInstance(_reactFlowInstance);
+	//Node element for a taken course
+	const CourseTaken = ({ data }) => {
+		return (
+			<div style={styles[0].taken}>
+				<Handle type="source" position="bottom" style={{ visibility: 'hidden' }} />
+				<div>{data.label}</div>
+				<Handle type="target" position="top" style={{ visibility: 'hidden' }} />
+			</div>
+		);
+	};
 
-  const onDragOver = (event) => {
-    event.preventDefault();
-    event.dataTransfer.dropEffect = 'move';
-  };
+	//Node element for a course that cannot be taken yet
+	const CourseCannotTake = ({ data }) => {
+		return (
+			<div style={styles[0].cannotTake}>
+				<Handle type="source" position="bottom" style={{ visibility: 'hidden' }} />
+				<div>{data.label}</div>
+				<Handle type="target" position="top" style={{ visibility: 'hidden' }} />
+			</div>
+		);
+	};
 
-  const onDrop = (event) => {
-    event.preventDefault();
+	//Node element for a course that has not been taken, but can be
+	const CourseCanTake = ({ data }) => {
+		return (
+			<div style={styles[0].canTake}>
+				<Handle type="source" position="bottom" style={{ visibility: 'hidden' }} />
+				<div>{data.label}</div>
+				<Handle type="target" position="top" style={{ visibility: 'hidden' }} />
+			</div>
+		);
+	};
 
-    const reactFlowBounds = reactFlowWrapper.current.getBoundingClientRect();
-    const type = event.dataTransfer.getData('application/reactflow');
-    const position = reactFlowInstance.project({
-      x: event.clientX - reactFlowBounds.left,
-      y: event.clientY - reactFlowBounds.top,
-    });
-    const newNode = {
-      id: getId(),
-      type,
-      position,
-      data: { label: `${type} node` },
-    };
+	// The 3 types of nodes that can appear in the Flow
+	const nodeTypes = {
+		courseTaken: CourseTaken,
+		courseCannotTake: CourseCannotTake,
+		courseCanTake: CourseCanTake
+	};
 
-    setElements((es) => es.concat(newNode));
-  };
+	//Handle dragging a node from the Sidebar
+	const onDragOver = (event) => {
+		event.preventDefault();
+		event.dataTransfer.dropEffect = 'move';
+	};
 
-  return (
-    <div className="dndflow" style={{height: 1080}}>
-    <ReactFlowProvider>
-    <div className="reactflow-wrapper" ref={reactFlowWrapper}>
-      <ReactFlow
-        elements={elements}
-        onConnect={onConnect}
-        onElementsRemove={onElementsRemove}
-        onLoad={onLoad}
-        onDrop={onDrop}
-        onDragOver={onDragOver}
-        snapToGrid={true}
-        snapGrid={[15, 15]}
-      >
-      <Background/>
-      </ReactFlow>
-      <Controls />
+	//Handle dropping the node from the sidebar adds the new node to the graph
+	const onDrop = (event) => {
+		event.preventDefault();
 
-    </div>
-  </ReactFlowProvider>
-  </div>
-  )
-}
+		const reactFlowBounds = reactFlowWrapper.current.getBoundingClientRect();
+		const type = event.dataTransfer.getData('application/reactflow');
+		const position = reactFlowInstance.project({
+			x: event.clientX - reactFlowBounds.left,
+			y: event.clientY - reactFlowBounds.top
+		});
+		const newNode = {
+			id: getId(),
+			type,
+			position,
+			data: { label: 'Course Node' }
+		};
 
-export default Flow
+		setElements((es) => es.concat(newNode));
+	};
+
+	return (
+		<div className="dndflow" style={{ height: 1080 }}>
+			<ReactFlowProvider>
+				<div className="reactflow-wrapper" ref={reactFlowWrapper}>
+					<ReactFlow
+						elements={elements}
+						nodeTypes={nodeTypes}
+						onConnect={onConnect}
+						onElementsRemove={onElementsRemove}
+						onLoad={onLoad}
+						onDrop={onDrop}
+						onDragOver={onDragOver}
+						snapToGrid={true}
+						snapGrid={[ 15, 15 ]}
+					/>
+					<Background gap={15} />
+					<Controls />
+				</div>
+			</ReactFlowProvider>
+		</div>
+	);
+};
+
+export default Flow;
--- client/src/components/GraphPage/Sidebar.js
+++ client/src/components/GraphPage/Sidebar.js
@@ -1,36 +1,43 @@
 import React from 'react';
-import {List, ListItem} from '@mui/material'
+import { List, ListItem } from '@mui/material';
+import { useState } from 'react';
 
-import './dnd.css'
+import './dnd.css';
+import CourseNodeStyles from './CourseNodeStyles';
 
 const Sidebar = () => {
-  const onDragStart = (event, nodeType) => {
-    event.dataTransfer.setData('application/reactflow', nodeType);
-    event.dataTransfer.effectAllowed = 'move';
-  };
+	const styles = useState(CourseNodeStyles);
+	const onDragStart = (event, nodeType) => {
+		event.dataTransfer.setData('application/reactflow', nodeType);
+		event.dataTransfer.effectAllowed = 'move';
+	};
 
-  return (
-    <List>
-      <ListItem button >
-      <div className="dndnode input" onDragStart={(event) => onDragStart(event, 'input')} draggable>
-        Input Node
-      </div>
-      </ListItem>
-      <ListItem button >
-
-      <div className="dndnode" onDragStart={(event) => onDragStart(event, 'default')} draggable>
-        Default Node
-      </div>
-      </ListItem>
-      <ListItem button >
-
-      <div className="dndnode output" onDragStart={(event) => onDragStart(event, 'output')} draggable>
-        Output Node
-      </div>
-      </ListItem>
-    </List>
-  );
+	return (
+		<List>
+			<ListItem button>
+				<div className="dndnode input" onDragStart={(event) => onDragStart(event, 'courseTaken')} draggable>
+					<div style={styles[0].taken}>
+						<div>Taken</div>
+					</div>
+				</div>
+			</ListItem>
+			<ListItem button>
+				<div className="dndnode" onDragStart={(event) => onDragStart(event, 'courseCannotTake')} draggable>
+					<div style={styles[0].cannotTake}>
+						<div>Cannot Take</div>
+					</div>
+				</div>
+			</ListItem>
+			<ListItem button>
+				<div className="dndnode output" onDragStart={(event) => onDragStart(event, 'courseCanTake')} draggable>
+					<div style={styles[0].canTake}>
+						<div>Can Take</div>
+					</div>
+				</div>
+			</ListItem>
+		</List>
+	);
 };
 
-export default Sidebar
+export default Sidebar;
 
