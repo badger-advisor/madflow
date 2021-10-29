66893acdbd17e10fa4db958ba1efd753fa24d886
Gene Lee <galee2@wisc.edu>
2021-10-28 22:42:14 -0500

    changes to display image and flow name

--- client/src/components/DashboardPage/FlowCard.js
+++ client/src/components/DashboardPage/FlowCard.js
@@ -1,21 +1,33 @@
 import { Card, CardMedia, CardHeader, Typography, Grid } from '@mui/material';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import testImg from './test.jpg';
 
 const FlowCard = ({ flowID, flowName }) => {
   return (
     <Card
       sx={{
-        width        : '100%',
-        height       : '100%',
-        margin       : '10px',
-        borderRadius : '16px'
+        width           : '100%',
+        height          : '100%',
+        borderRadius    : '16px',
+        backgroundColor : '#0A9396'
       }}
       variant='outlined'
       borderradius={16}
       block='flex'
     >
-      <CardMedia component='img' height='50px' image='/test.png' alt='test image' />
-      <div align='center' sx={{ backgroundColor: '#0A9396' }}>
-        <Typography>{flowName}</Typography>
+      <div align='center'>
+        <CardMedia component='img' height='50%' image={testImg} alt='test image' />
+        <Typography
+          sx={{
+            height         : '75px',
+            color          : 'white',
+            display        : 'flex',
+            justifyContent : 'center',
+            flexDirection  : 'column'
+          }}
+        >
+          {flowName}
+        </Typography>
       </div>
     </Card>
   );
