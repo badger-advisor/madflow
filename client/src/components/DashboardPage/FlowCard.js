3aa43ae04fdfff90df91f2b2f2e6e2ef496d60cb
Gene Lee <galee2@wisc.edu>
2021-10-29 00:33:41 -0500

    adds menu to FlowCard

--- client/src/components/DashboardPage/FlowCard.js
+++ client/src/components/DashboardPage/FlowCard.js
@@ -1,13 +1,28 @@
-import { Card, CardMedia, Typography, Avatar, alpha } from '@mui/material';
 import { useState } from 'react';
+
+// material-ui
+import { Card, CardMedia, Typography, Avatar, alpha, Menu, MenuItem } from '@mui/material';
+
+// icons and images
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import DriveFileRenameOutlineIcon from '@mui/icons-material/DriveFileRenameOutline';
+import FileCopyTwoToneIcon from '@mui/icons-material/FileCopyOutlined';
+import DeleteIcon from '@mui/icons-material/Delete';
 import testImg from './test.jpg';
 
 const FlowCard = ({ flowID, flowName }) => {
-  // TODO: function to handle opening the menu when the '...' icon is clicked
+  const [ showMenu, setShowMenu ] = useState(null);
+
   const openMenu = (e) => {
     e.stopPropagation(); // stop openFlow() from being called when menu icon is clicked
-    console.log('button clicked!');
+    console.log('menu clicked!');
+    setShowMenu(e.currentTarget);
+  };
+
+  const closeMenu = (e) => {
+    e.stopPropagation();
+    console.log('close menu');
+    setShowMenu(null);
   };
 
   // TODO: function to handle opening a Flow when the FlowCard is clicked
@@ -29,6 +44,7 @@ const FlowCard = ({ flowID, flowName }) => {
       block='flex'
       onClick={openFlow}
     >
+      {/* FlowCard '...' menu icon */}
       <Avatar // TODO: Move to top right corner of Card
         variant='rounded'
         sx={{
@@ -40,7 +56,42 @@ const FlowCard = ({ flowID, flowName }) => {
       >
         <MoreHorizIcon sx={{ color: 'gray', cursor: 'pointer' }} />
       </Avatar>
+
+      {/* FlowCard menu Popover */}
+      <Menu
+        id='menu'
+        anchorEl={showMenu}
+        keepMounted
+        open={Boolean(showMenu)}
+        onClose={closeMenu}
+        variant='selectedMenu'
+        anchorOrigin={{
+          vertical   : 'bottom',
+          horizontal : 'right'
+        }}
+        transformOrigin={{
+          vertical   : 'top',
+          horizontal : 'right'
+        }}
+      >
+        <MenuItem onClick={closeMenu}>
+          <DriveFileRenameOutlineIcon sx={{ mr: 1.75 }} />
+          Rename Flow
+        </MenuItem>
+        <MenuItem onClick={closeMenu}>
+          <FileCopyTwoToneIcon sx={{ mr: 1.75 }} />
+          Copy to New Flow
+        </MenuItem>
+        <MenuItem onClick={closeMenu}>
+          <DeleteIcon sx={{ mr: 1.75 }} />
+          Delete Flow
+        </MenuItem>
+      </Menu>
+
+      {/* FlowCard image */}
       <CardMedia component='img' height='80%' image={testImg} alt='test image' />
+
+      {/* Flow name */}
       <Typography
         sx={{
           height         : '20%',
