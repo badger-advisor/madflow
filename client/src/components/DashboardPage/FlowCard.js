4cee249e8c118151af87b5f3e7c97f138edc0b9e
Gene Lee <galee2@wisc.edu>
2021-10-29 00:47:16 -0500

    create FlowCardMenu with dummy menu functions

--- client/src/components/DashboardPage/FlowCard.js
+++ client/src/components/DashboardPage/FlowCard.js
@@ -1,5 +1,8 @@
 import { useState } from 'react';
 
+// components
+import FlowCardMenu from './FlowCardMenu';
+
 // material-ui
 import { Card, CardMedia, Typography, Avatar, alpha, Menu, MenuItem } from '@mui/material';
 
@@ -17,12 +20,6 @@ const FlowCard = ({ flowID, flowName }) => {
     e.stopPropagation(); // stop openFlow() from being called when menu icon is clicked
     console.log('menu clicked!');
     setShowMenu(e.currentTarget);
-  };
-
-  const closeMenu = (e) => {
-    e.stopPropagation();
-    console.log('close menu');
-    setShowMenu(null);
   };
 
   // TODO: function to handle opening a Flow when the FlowCard is clicked
@@ -58,35 +55,7 @@ const FlowCard = ({ flowID, flowName }) => {
       </Avatar>
 
       {/* FlowCard menu Popover */}
-      <Menu
-        id='menu'
-        anchorEl={showMenu}
-        keepMounted
-        open={Boolean(showMenu)}
-        onClose={closeMenu}
-        variant='selectedMenu'
-        anchorOrigin={{
-          vertical   : 'bottom',
-          horizontal : 'right'
-        }}
-        transformOrigin={{
-          vertical   : 'top',
-          horizontal : 'right'
-        }}
-      >
-        <MenuItem onClick={closeMenu}>
-          <DriveFileRenameOutlineIcon sx={{ mr: 1.75 }} />
-          Rename Flow
-        </MenuItem>
-        <MenuItem onClick={closeMenu}>
-          <FileCopyTwoToneIcon sx={{ mr: 1.75 }} />
-          Copy to New Flow
-        </MenuItem>
-        <MenuItem onClick={closeMenu}>
-          <DeleteIcon sx={{ mr: 1.75 }} />
-          Delete Flow
-        </MenuItem>
-      </Menu>
+      <FlowCardMenu showMenu={showMenu} setShowMenu={setShowMenu} />
 
       {/* FlowCard image */}
       <CardMedia component='img' height='80%' image={testImg} alt='test image' />
