66299a74e1b439d2676af6b0b7c4070003adc40b
Gene Lee <galee2@wisc.edu>
2021-10-28 23:24:54 -0500

    adds menu button to FlowCard

--- client/src/components/DashboardPage/FlowCard.js
+++ client/src/components/DashboardPage/FlowCard.js
@@ -1,8 +1,13 @@
-import { Card, CardMedia, CardHeader, Typography, Grid } from '@mui/material';
+import { Card, CardMedia, Typography, Avatar, alpha } from '@mui/material';
+import { useState } from 'react';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import testImg from './test.jpg';
 
 const FlowCard = ({ flowID, flowName }) => {
+  const openMenu = (e) => {
+    console.log('button clicked!');
+  };
+
   return (
     <Card
       sx={{
@@ -15,20 +20,30 @@ const FlowCard = ({ flowID, flowName }) => {
       borderradius={16}
       block='flex'
     >
-      <div align='center'>
-        <CardMedia component='img' height='50%' image={testImg} alt='test image' />
-        <Typography
-          sx={{
-            height         : '75px',
-            color          : 'white',
-            display        : 'flex',
-            justifyContent : 'center',
-            flexDirection  : 'column'
-          }}
-        >
-          {flowName}
-        </Typography>
-      </div>
+      <Avatar // TODO: Move to top right corner of Card
+        variant='rounded'
+        sx={{
+          position        : 'absolute',
+          backgroundColor : alpha('#7C7C7C', 0.2),
+          zIndex          : 1
+        }}
+        onClick={openMenu}
+      >
+        <MoreHorizIcon sx={{ color: 'gray', cursor: 'pointer' }} />
+      </Avatar>
+      <CardMedia component='img' height='80%' image={testImg} alt='test image' />
+      <Typography
+        sx={{
+          height         : '20%',
+          color          : 'white',
+          display        : 'flex',
+          justifyContent : 'center',
+          flexDirection  : 'column',
+          alignItems     : 'center'
+        }}
+      >
+        {flowName}
+      </Typography>
     </Card>
   );
 };
