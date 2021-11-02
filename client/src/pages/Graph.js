dee00d229fda95714e90beabab79315e2a18ea91
James Zhang <jameszhanga@gmail.com>
2021-11-02 11:27:11 -0500

    Redo works as expected

--- client/src/pages/Graph.js
+++ client/src/pages/Graph.js
@@ -1,20 +1,16 @@
-import React, { useState, useEffect, forceUpdate } from 'react';
+import React, { useState, useEffect } from 'react';
 import { styled, useTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
 
-import SearchNavBar from '../components/GraphPage/SearchNavBar';
 import '../components/GraphPage/dnd.css';
-import DrawerHeader from '../components/GraphPage/HeaderSpacer';
 import Flow from '../components/GraphPage/Flow';
-import Sidebar from '../components/GraphPage/Sidebar';
-
+import SearchNavBar from '../components/GraphPage/SearchNavBar';
+import DrawerHeader from '../components/GraphPage/HeaderSpacer';
 import RecommendBar from '../components/GraphPage/RecommendBar';
 
+// TODO initialElements to be replaced with fetched elements from DB
 import { initialElements } from './initialElements';
-
-import { removeElements } from 'react-flow-renderer';
 
 export const RECOMMEND_BAR_WIDTH = 240;
 
@@ -37,6 +33,8 @@ const Main = styled('main', { shouldForwardProp: prop => prop !== 'open' })(({ t
 
 const Graph = () => {
   const [ openRec, setOpenRec ] = useState(false);
+  // All of the data for the Flow
+  // TODO initialElements to be replaced with fetched elements from DB
   const [ elements, setElements ] = useState(initialElements);
 
   // Undo stack consists of a list of all element states
@@ -44,38 +42,48 @@ const Graph = () => {
   // Each time before the undo stack is popped, the current state added to the redo stack
   const [ redo, setRedo ] = useState([]);
 
-  // localStorage.setItem('elements', JSON.stringify(elements));
+  // for making sure the elements array update each time undo or redo is applied
+  useEffect(
+    () => {
+      setElements(undo[undo.length - 1]);
+    },
+    [ undo ]
+  );
 
-  // useEffect(
-  //   () => {
-  //     localStorage.setItem('elements', JSON.stringify(elements));
-  //   },
-  //   [ elements ]
-  // );
-
+  /**
+   * Set function called each time redo is applied
+   */
   const handleRedo = () => {
     // Redo base is 0 because there should be nothing in there until user undos
     if (redo.length === 0) return;
+
+    let temp = [ ...redo ];
+    const newState = temp.pop();
+
+    saveForUndo(newState);
+    // This HAS to be called after saveForUndo, otherwise the redo stack will be empty
+    setRedo(temp);
   };
 
+  /**
+   * Actions that are tracked for redo:
+   ** 1. Moving node
+   *  2. Adding node
+   *  3. Removing node
+   *  4. generate prereq
+   */
   const handleUndo = () => {
     // undo base is 1 because it should initialize with the default state in there
     if (undo.length === 1) return;
 
-    // Then apply the previous state to the current elements
+    // Make a copy of the state that will be mutated
     let temp = [ ...undo ];
-    console.log('temp');
-    console.log(temp);
 
     const currState = temp.pop();
-    console.log('curr state');
-    console.log(currState);
 
-    // First save the current state
+    // Save for redo stack and apply the new elements
     saveForRedo(currState);
-
     setUndo(temp);
-    setElements(temp[temp.length - 1]);
   };
 
   /**
@@ -83,6 +91,8 @@ const Graph = () => {
    * @param {Array} newEle The elementes array
    */
   const saveForUndo = newEle => {
+    console.log('set redo empty');
+    setRedo([]);
     setUndo(prev => prev.concat([ newEle ]));
   };
 
