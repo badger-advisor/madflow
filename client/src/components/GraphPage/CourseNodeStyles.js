bfeaa91630d1358f0380ad954b4cc7529c053777
gjsorrell <gregorysorrell@gmail.com>
2021-11-03 18:44:36 -0500

    Reformat

--- client/src/components/GraphPage/CourseNodeStyles.js
+++ client/src/components/GraphPage/CourseNodeStyles.js
@@ -1,51 +1,51 @@
 const CourseNodeStyles = () => {
-	return {
-		taken: {
-			margin: 'auto',
-			display: 'flex',
-			justifyContent: 'center',
-			alignItems: 'center',
-			background: '#FFFFFF',
-			color: '#000000',
-			border: '2px solid #484848',
-			borderRadius: 5,
-			fontSize: 12,
-			width: 105,
-			height: 45,
-			textAlign: 'center',
-			targetPosition: 'top',
-			sourcePosition: 'bottom'
-		},
-		cannotTake: {
-			align: 'center',
-			display: 'flex',
-			justifyContent: 'center',
-			alignItems: 'center',
-			background: '#FFFFFF',
-			color: '#000000',
-			border: '2px solid #a33d3d',
-			borderRadius: 5,
-			fontSize: 12,
-			width: 105,
-			height: 45,
-			textAlign: 'center'
-		},
-		canTake: {
-			align: 'center',
-			display: 'flex',
-			justifyContent: 'center',
-			alignItems: 'center',
-			background: '#FFFFFF',
-			color: '#000000',
-			padding: 1,
-			border: '2px solid #008000',
-			borderRadius: 5,
-			fontSize: 12,
-			width: 105,
-			height: 45,
-			textAlign: 'center'
-		}
-	};
+  return {
+    taken      : {
+      margin         : 'auto',
+      display        : 'flex',
+      justifyContent : 'center',
+      alignItems     : 'center',
+      background     : '#FFFFFF',
+      color          : '#000000',
+      border         : '2px solid #484848',
+      borderRadius   : 5,
+      fontSize       : 12,
+      width          : 105,
+      height         : 45,
+      textAlign      : 'center',
+      targetPosition : 'top',
+      sourcePosition : 'bottom'
+    },
+    cannotTake : {
+      align          : 'center',
+      display        : 'flex',
+      justifyContent : 'center',
+      alignItems     : 'center',
+      background     : '#FFFFFF',
+      color          : '#000000',
+      border         : '2px solid #a33d3d',
+      borderRadius   : 5,
+      fontSize       : 12,
+      width          : 105,
+      height         : 45,
+      textAlign      : 'center'
+    },
+    canTake    : {
+      align          : 'center',
+      display        : 'flex',
+      justifyContent : 'center',
+      alignItems     : 'center',
+      background     : '#FFFFFF',
+      color          : '#000000',
+      padding        : 1,
+      border         : '2px solid #008000',
+      borderRadius   : 5,
+      fontSize       : 12,
+      width          : 105,
+      height         : 45,
+      textAlign      : 'center'
+    }
+  };
 };
 
 export default CourseNodeStyles;
