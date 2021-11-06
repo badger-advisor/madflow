883862e2e10987ef006769613209c7491b12653b
gjsorrell <gregorysorrell@gmail.com>
2021-11-05 23:24:58 -0500

    Merge with dev, initial elements have fake prereqs

--- client/src/pages/initialElements.js
+++ client/src/pages/initialElements.js
@@ -2,7 +2,14 @@ export const initialElements = [
   {
     id       : '1',
     type     : 'courseTaken',
-    data     : { label: 'Welcome to MadFlow!' },
+    data     : {
+      courseName    : 'Welcome',
+      credits       : '3',
+      description   : 'Welcome to the site! Please enjoy your stay.',
+      label         : 'Welcome to MadFlow!',
+      lastTaught    : 'Forever',
+      prerequisites : [ '' ]
+    },
     position : { x: 795, y: 105 }
   },
   {
@@ -15,20 +22,34 @@ export const initialElements = [
         'The third course in our programming fundamentals sequence. It presumes that students understand and use functional and object-oriented design and abstract data types as needed. This course introduces balanced search trees, graphs, graph traversal algorithms, hash tables and sets, and complexity analysis and about classes of problems that require each data type. Students are required to design and implement using high quality professional code, a medium sized program, that demonstrates knowledge and use of latest language features, tools, and conventions. Additional topics introduced will include as needed for projects: inheritance and polymorphism; anonymous inner classes, lambda functions, performance analysis to discover and optimize critical code blocks. Students learn about industry standards for code development. Students will design and implement a medium size project with a more advanced user-interface design, such as a web or mobile application with a GUI and event- driven implementation; use of version-control software. Enroll Info: None',
       label         : 'CS400',
       lastTaught    : 'Fall 2021',
-      prerequisites : [ 'CS300' ]
+      prerequisites : [ '1' ]
     },
     position : { x: 690, y: 210 }
   },
   {
     id       : '3',
     type     : 'courseCanTake',
-    data     : { label: 'CS240' },
+    data     : {
+      courseName    : 'Programming II',
+      credits       : '3',
+      description   : 'This is the CS 240 course',
+      label         : 'CS240',
+      lastTaught    : 'Fall 2021',
+      prerequisites : [ '1' ]
+    },
     position : { x: 900, y: 210 }
   },
   {
     id       : '4',
     type     : 'courseCannotTake',
-    data     : { label: 'CS577' },
+    data     : {
+      courseName    : 'Algorithms',
+      credits       : '3',
+      description   : 'This is the CS 577 course',
+      label         : 'CS577',
+      lastTaught    : 'Fall 2021',
+      prerequisites : [ 'CS400', '3' ]
+    },
     position : { x: 795, y: 315 }
   },
 
