8aef2b90a35dd67b552c3fe95efbf8885d7f1667
JaskaranBakshi <jaskaran@quiverquant.com>
2021-11-17 22:59:38 -0600

    changes

--- clean_course_data.py
+++ clean_course_data.py
@@ -33,49 +33,54 @@ def run():
     des = k['description']
     title = k['title']
     pre_req = k['enrollmentPrerequisites']
-    all_pre = []
+    all_pre = set()
     for j in key_major_codes:
         for i in findall(j, pre_req):
             if pre_req[i+len(j):i+len(j)+10] == '/MATH/STAT':
                 pre = pre_req[i:i+len(j)] + " " + pre_req[i+len(j)+11:i+len(j)+14] 
-                all_pre.append(pre)
+                all_pre.update([pre])
                 pre = pre_req[i+len(j)+1:i+len(j)+5] + " " + pre_req[i+len(j)+11:i+len(j)+14] 
-                all_pre.append(pre)
+                all_pre.update([pre])
                 pre = pre_req[i+len(j)+6:i+len(j)+10] + " " + pre_req[i+len(j)+11:i+len(j)+14] 
-                all_pre.append(pre)
+                all_pre.update([pre])
             elif pre_req[i+len(j):i+len(j)+5] == '/MATH' or pre_req[i+len(j):i+len(j)+5] == '/STAT':
                 pre = pre_req[i:i+len(j)] + " " + pre_req[i+len(j)+6:i+len(j)+9] 
-                all_pre.append(pre)
+                all_pre.update([pre])
                 pre = pre_req[i+len(j)+1:i+len(j)+5] + " " + pre_req[i+len(j)+6:i+len(j)+9] 
-                all_pre.append(pre)
+                all_pre.update([pre])
             elif pre_req[i+len(j):i+len(j)+6] == '/E C E':  
+                # could not think of a better way of handling cases such as the folloiwng:
+                # COMP SCI/E C E 552
+                # or
+                # (COMP SCI/MATH 240 or COMP SCI/MATH/STAT 475)
+                # if you figure it out fix it here
                 pre = pre_req[i:i+len(j)] + " " + pre_req[i+len(j)+7:i+len(j)+10] 
-                all_pre.append(pre)
+                all_pre.update([pre])
                 pre = pre_req[i+len(j)+1:i+len(j)+6] + " " + pre_req[i+len(j)+7:i+len(j)+10] 
-                all_pre.append(pre)
+                all_pre.update([pre])
             elif pre_req[i+len(j)+5:i+len(j)+7] == 'or':
                 try:
                     temp = int(pre_req[i+len(j)+8:i+len(j)+11])
-                    all_pre.append(j + " " + str(temp) )
+                    all_pre.update([j + " " + str(temp) ])
                 except:
                     continue
             elif pre_req[i+len(j)+5:i+len(j)+8] == 'and':
                 try:
                     temp = int(pre_req[i+len(j)+9:i+len(j)+12])
-                    all_pre.append(j + " " + str(temp) )
+                    all_pre.update([j + " " + str(temp)] )
                 except:
                     continue
             elif pre_req[i+len(j)+5:i+len(j)+6] == ',':
                 mult = helper(j,pre_req, i+len(j)+7)
                 for b in mult:
-                    all_pre.append(b)
+                    all_pre.update([b])
             else:
                 pre = pre_req[i:i+len(j)+4]
-                all_pre.append(pre)
+                all_pre.update([pre])
     for j in outside_major:
         if j in pre_req:
-            all_pre.append(j)
-    row = {"courseNumber":course_name,"info":{"courseName": title, "description":des, "credits":credit, "lastTaught":lt}, "prerequisites":all_pre}
+            all_pre.update([j])
+    row = {"courseNumber":course_name,"info":{"courseName": title, "description":des, "credits":credit, "lastTaught":lt}, "prerequisites":list(all_pre)}
     data_for_mongo.append(row)
   print(data_for_mongo)
   return(data_for_mongo)
