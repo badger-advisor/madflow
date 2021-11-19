5a0519710e42e8da69bcfb38cfe4afb54e930cb6
James Zhang <jameszhanga@gmail.com>
2021-11-18 21:45:56 -0600

    Code format

--- tests/flow.test.js
+++ tests/flow.test.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
-const app = require('../server');
 const supertest = require('supertest');
-
+const app = require('../server');
 require('dotenv').config();
+
 const db = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
 // Setup and teardown
 beforeAll(async () => await mongoose.connect(db), 60 * 1000); // give 60 seconds to connect to db, default 5
 afterAll(async () => await mongoose.connection.close());
+
+/** #################### tests begin below #################### */
 
 describe('Testing flow controller functions', () => {
   let flowID = '';
