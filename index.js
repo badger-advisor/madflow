a475722047dc687ebbb296d7d89be3457e89858a
James Zhang <jameszhanga@gmail.com>
2021-11-28 16:05:57 -0600

    Configure to serve frontend

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 const app = require('./server');
+const path = require('path');
+const express = require('express');
+
+// Serve app if in production
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolv(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // Start server, listen for requests
 const PORT = process.env.PORT || 8080;
