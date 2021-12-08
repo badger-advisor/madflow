52e074aa357fce64c92c51f217bc3d0fcb156d85
James Zhang <jameszhanga@gmail.com>
2021-12-07 19:26:33 -0600

    Landing page flow up

--- client/src/components/LandingPage/SidePanel.js
+++ client/src/components/LandingPage/SidePanel.js
@@ -5,28 +5,6 @@ import { useContext } from 'react';
 import UserProvider from '../../contexts/UserProvider';
 import { signIn } from '../../api';
 
-const useStyles = makeStyles(() => {
-  return {
-    root      : {
-      // idk how this works, but it targets the root of the drawer component
-      '& .MuiPaper-root' : {
-        backgroundColor : alpha('#FFE2A8', 0.8),
-        borderRadius    : '0 15px 15px 0',
-        boxShadow       : '2px 5px 5px 0px rgba(0,0,0,0.25);',
-        justifyContent  : 'center',
-        width           : '30vw'
-      }
-    },
-    sidePanel : {
-      display : 'flex'
-    },
-    button    : {
-      pb : '20px',
-      pt : '20px'
-    }
-  };
-});
-
 /**
  * if there is no current user, the top button will direct the user
  * to google OAuth
@@ -55,16 +33,20 @@ const DashButton = () => {
   );
 };
 
-const SidePanel = () => {
+const SidePanel = ({ classes }) => {
   const { loggedIn } = useContext(UserProvider.context);
-  const classes = useStyles();
+  // const classes = useStyles();
 
   return (
     <Drawer
-      className={classes.sidePanel}
-      anchor='left'
+      // className={classes.sidePanel}
+      // anchor='left'
+      // variant='permanent'
+      // classes={{ paper: classes.drawerPaper, root: classes.root }}
+      className={classes.drawer}
       variant='permanent'
-      classes={{ paper: classes.drawerPaper, root: classes.root }}
+      classes={{ paper: classes.drawerPaper, root: classes.drawerRoot }}
+      anchor='left'
     >
       <Typography variant='h3' align='center'>
         Welcome to
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,7 +1,53 @@
 import SidePanel from '../components/LandingPage/SidePanel';
+import { makeStyles } from '@material-ui/core';
+import LandingFlow from '../components/LandingPage/LandingFlow';
+
+const drawerWidth = '30vw';
+
+const useStyles = makeStyles({
+  paper       : {},
+  page        : {
+    background : '#f9f9f9',
+    width      : '100%'
+  },
+  root        : {
+    display : 'flex'
+  },
+  drawer      : {
+    width : drawerWidth
+  },
+  drawerPaper : {
+    width          : drawerWidth,
+    borderRadius   : '0 15px 15px 0',
+    justifyContent : 'center'
+  },
+  drawerRoot  : {
+    '& .MuiPaper-root' : {
+      // backgroundColor : 'rgba(129, 233, 255, 0.8)',
+      background : 'rgb(63,94,251)',
+      background : 'linear-gradient(180deg, rgba(63,94,251,1) 0%, rgba(252,70,107,1) 100%)',
+      boxShadow  : '0px 0 5px 0 rgba(0,0,0,0.25);'
+    }
+  }
+});
+
+const LandingFlowx = () => {
+  return (
+    <div style={{ width: '100%', height: '100vh', padding: '0', margin: '0' }}>
+      <LandingFlow />
+    </div>
+  );
+};
 
 const Landing = () => {
-  return <SidePanel />;
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <SidePanel classes={classes} />
+      <LandingFlowx />
+    </div>
+  );
 };
 
 export default Landing;
