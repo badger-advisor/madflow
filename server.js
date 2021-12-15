afab87340526df3e881c2888872d52919a2c0da9
James Zhang <jameszhanga@gmail.com>
2021-12-15 02:25:26 -0600

    Hot fix production resolve

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolv(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
